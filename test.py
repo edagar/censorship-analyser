10ae93fa41d5de308c1a43bab8b57dfa0745da63
edagar <tobiasrang@gmail.com>
2014-07-10 17:19:13 +0200

    Add TestParser
    The TestParser class knows how to parse the output produced by the
    ooniprobe tests. The idea is to seperate the parsing logic from the Test
    class.

--- test.py
+++ test.py
@@ -3,29 +3,70 @@ from random import choice, randint
 from time import sleep
 
 class Test(object):
-    def __init__(self, testfile, args, ooni_binary):
+
+    def __init__(self, testfile, args=[]):
         self.testfile = testfile
         self.args = args
-        self.binary = ooni_binary
         self.output = None 
         self.status = None
+        self.errorMessage = None
+        self.parser = None
 
     def run(self):
-        if self.args:
-            args = (self.binary, "-n", self.testfile, self.args[0], self.args[1])
-        else:
-            args = (self.binary, "-n", self.testfile)
-        print "Running test %s" % self.testfile
-        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-        popen.wait()
-        self.parseOutput(popen.stdout.read())
+        self.output = runTest(self)
+        self.parseResults()
+
+    def parseResults(self):
+        self.parser = TestParser(self)
+        self.parser.parseOutput(self.output)
+
+    def printResults(self):
+        self.parser.printResults()
+
+    def getResults(self):
+        return {
+                "Status": self.status,
+                "ErrorMessage": self.errorMessage,
+                }
+
+
+class TestParser(object):
+    def __init__(self, test):
+        self.test = test
+
+    def findValue(self, key):
+        """
+        The ooniprobe tests include simple key/value pairs
+        in their output, indicating the test results. 
+        
+        To avoid false positives, the format "key: [ VALUE ]"
+        is used. This method takes "key" as a parameter,
+        and returns VALUE.
+        """
+        output = self.test.output
+        if not key in output:
+            return "NOT FOUND"
+
+        start = output.find(key) + len(key)  
+        value = output[start:].split(" ]")[0][1:].strip()    
+        return value
 
     def parseOutput(self, output):
-        self.output = output
-        key = "status: "
-        start = output.find(key) + len(key)  
-        status = output[start:].split(" ")[0]
-        self.status = status
+        self.test.status = self.findValue("TestStatus: ")
+        if not self.test.status == "OK":
+            self.test.errorMessage = self.findValue("TestException: ")
+    
+    def printResults(self):
+        print "Test: %s" % self.test.testfile
+        if hasattr(self.test, "target"):
+            print "Target: %s" % self.test.target
+
+        results = self.test.getResults()
+        for key, value in results.iteritems():
+            if key and value:
+                print "%s: %s" % (key, value)
+
+
 
 class TestCase(object):
     def __init__(self, tests=[], sleep_interval=(1,20)):
@@ -44,3 +85,16 @@ class TestCase(object):
         for test in self.tests:
             print test.status 
 
+def runTest(test):
+    binary = "ooniprobe"
+    args = [binary, "-n", test.testfile]
+
+    if test.args:
+        args += test.args
+        
+    print "Running test %s" % test.testfile
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+
+    output = popen.stdout.read()
+    return output
