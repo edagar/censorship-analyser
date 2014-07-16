01d3d742017cda3e02e535188d6035d9f552cfdf
edagar <tobiasrang@gmail.com>
2014-07-16 19:14:28 +0000

    Update functions to make use of the new const.py module.

--- analyser.py
+++ analyser.py
@@ -3,29 +3,9 @@
 import subprocess
 from time import sleep
 from random import randint, choice
-from test import *
 
-SLEEP_INTERVAL = (1, 25)  # Interval for random sleep periods between tests.
-OONI_BINARY = "ooniprobe"
-PROBE_TEST = "siteprobe.py"
-DNS_TEST = "dnscompare.py"
-TCP_TEST = "tcpconnect.py"
-PING_TEST = "ping.py"
-TRACEROUTE_TEST = "traceroute.py"
-TOR_SITE_URL = "https://www.torproject.org"
-TOR_BRIDGES_URL =  "bridges.torproject.org"
-TOR_MIRRORS = ["https://www.unicorncloud.org/public/torproject.org/", "https://mirror.ml/tor/", "https://www.oignon.net/", "https://tor.hackthissite.org/"]
-TOR_DIRECTORY_AUTHORITIES = [
-                             { "128.31.0.34": 9131 },
-                             { "86.59.21.38": 80 },
-                             { "194.109.206.212": 80 },
-                             { "76.73.17.194": 9030 },
-                             { "212.112.245.170": 80 },
-                             { "193.23.244.244": 80 },
-                             { "208.83.223.34": 443 }, 
-                             { "171.25.193.9": 443 },
-                             { "154.35.32.5": 80 },
-                            ]
+from test import *
+from const import *
     
 
 def probeTorSite():
@@ -47,9 +27,7 @@ def probeTorSite():
         print "Tor site reachable"
          
 def probeDirectoryAuthorities():
-    for server in TOR_DIRECTORY_AUTHORITIES:
-        host = server.keys()[0]
-        port = server[host] 
+    for host, port in TOR_DIRECTORY_AUTHORITIES.iteritems():
         url = makeURL(host, port)
 
         if siteReachable(url):
